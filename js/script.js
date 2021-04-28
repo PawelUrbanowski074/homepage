b1e84cc5c6f6ac726aeb113e523bbfa5153c3db2
palel <32599663+palel@users.noreply.github.com>
2021-04-28 22:07:06 +0200

    js function added

--- js/script.js
+++ js/script.js
@@ -1,39 +1,45 @@
-console.log("Witaj nieznany przybyszu!");
-
-let image = document.querySelector(".js-image");
-let imageButton = document.querySelector(".js-imageButton");
-
-imageButton.addEventListener("click", () => {
-    image.classList.toggle("section__image--small");
-    image.classList.toggle("section__image");
-    imageButton.remove();
-});
-
-let interestsButton = document.querySelector(".js-interestsButton");
-let interests = document.querySelector(".js-interests");
-
-interestsButton.addEventListener("click", () => {
-    interests.classList.toggle("hidden");
-    interestsButton.innerText === "Wyświetl" ? interestsButton.innerText = "Schowaj" :
-        interestsButton.innerText = "Wyświetl"
-});
-
-let interestsNav = document.querySelector(".js-interestsNav");
-
-interestsNav.addEventListener("click", () => {
-    if (interests.classList.contains("hidden")) {
-        interests.classList.toggle("hidden")
-        interestsButton.innerText === "Wyświetl" ? interestsButton.innerText = "Schowaj" :
-            interestsButton.innerText = "Wyświetl"
+{
+    welcome = () => {
+        console.log("Witaj nieznany przybyszu!");
     }
-});
 
-let instrumentsNav = document.querySelector(".js-instrumentsNav");
-
-instrumentsNav.addEventListener("click", () => {
-    if (interests.classList.contains("hidden")) {
-        interests.classList.toggle("hidden")
-        interestsButton.innerText === "Wyświetl" ? interestsButton.innerText = "Schowaj" :
-            interestsButton.innerText = "Wyświetl"
+    const imageButton = document.querySelector(".js-imageButton");
+    
+    const imageButtonClick = () => {
+        const image = document.querySelector(".js-image");
+        image.classList.toggle("section__image--small");
+        image.classList.toggle("section__image");
+        imageButton.remove();
     }
-});
+
+    const interestsButton = document.querySelector(".js-interestsButton");
+    const interests = document.querySelector(".js-interests");
+
+    const toggleButton = () => {
+        interests.classList.toggle("hidden");
+        interestsButton.innerText === "Wyświetl" ? interestsButton.innerText = "Schowaj" : interestsButton.innerText = "Wyświetl"
+    }
+
+    const isInterestsHidden = () => {
+        if (interests.classList.contains("hidden")) {
+            toggleButton();
+        }
+    }
+
+    const interestsButtonClick = () => {
+        toggleButton();
+    }
+
+    const init = () => {
+        const interestsNav = document.querySelector(".js-interestsNav");
+        const instrumentsNav = document.querySelector(".js-instrumentsNav");
+
+        imageButton.addEventListener("click", imageButtonClick);
+        interestsButton.addEventListener("click", interestsButtonClick);
+        interestsNav.addEventListener("click", isInterestsHidden);
+        instrumentsNav.addEventListener("click", isInterestsHidden);
+        welcome();
+    }
+
+    init();
+}
